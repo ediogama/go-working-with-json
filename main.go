f1b3fc9df64d53fbb4608eae111ccd7b0cd4f57b
Edio Gama <ediogama@hotmail.com>
2022-10-08 20:33:27 -0300

    add json with marshal

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "encoding/json"
 
+type Account struct {
+	Number  int
+	Balance int
+}
+
+func main() {
+	account := Account{Number: 1, Balance: 100}
+	res, err := json.Marshal(account)
+	if err != nil {
+		println(err)
+	}
+	println(string(res))
 }
